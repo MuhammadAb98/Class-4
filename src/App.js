fcdd416bd72acf0351903f75c131f35655ef7153
MuhammadAb98 <56094500+MuhammadAb98@users.noreply.github.com>
2020-11-04 18:22:50 +0500

    CLASS 4 COMPLETE
    1) How to make a variable with Hook
    2) how to use ternary operator
    3) Button onClick={()=>}

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, {useState} from 'react';
+import {Msg} from './mess';
+import './App.css';
 
 
 function Main()
 {
 
 let [count,setCount]=useState(0) // hook initialize variable 
+let [isMorning,setmorning]=useState(true);
 
 return(
-  <div>
-<h1>Hello {count}</h1>
-<button onClick={
-  ()=>alert('Button Pressed')}>   
-  Updates Counter</button>
-    
+  
+  <div className={`box ${isMorning?'DayLight':''}`}>
 
+    <h6>Day Time {isMorning ? 'Morning':'Night' }</h6>
+
+   <Msg co={count} />
+<button className="btn" onClick={  ()=>setCount(++count)  }   >   
+
+  Increment</button> 
+  <br/>
+
+  <button className="btn" onClick={
+
+    
+    ()=>setCount(--count)
+    
+    }>
+    Decrement
+  </button>
+
+<br/>
+  <button className="btn" onClick={
+
+    
+()=>setCount(0)
+
+}>
+RESET
+</button>
+<br/>
+<button className="btn"
+onClick={
+  ()=> setmorning(!isMorning)  //isMorning is waja se likha kuin kay changing 
+                               //isi pay ani ha
+}
+
+>
+Change Theme
+</button>
 </div>
 );  //Ye Arrow function anamous hota ha with no name
 }
---
+++ src/mess.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Msg (props)
+{
+    return(
+    <h3>Counter Value: {props.co}</h3>
+    );
+
+}
